b117fd83d5326d72d3826a73115473d6e62af788
Jose Luis <sisjosex@gmail.com>
2022-04-07 08:26:42 -0400

    advances

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,11 @@
 import TokenSummaryCard from '../../Components/Cards/TokenSummaryCard'
 import './style.scss'
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { useContext } from 'react'
 import { AuthenticateContext } from '../../Context/Auth'
+import WalletBalance from "../../Components/Cards/WalletBalance";
+import { Row, Col } from 'antd';
+import Copy from "../../Components/Page/Copy";
 
 function Home(props) {
 
@@ -13,9 +16,16 @@ function Home(props) {
             <h1 className="PageTitle">Home</h1>
             <h3 className="PageSubTitle">Keep calm and Hodl on</h3>
             <div className="WalletCardsContainer">
-                <TokenSummaryCard tokenName="stable" color="#00a651" page="/wallet/stable" colorAmount={""} />
-                <TokenSummaryCard tokenName="riskpro" color="#ef8a13" page="/wallet/pro" colorAmount={""} />
-                <TokenSummaryCard tokenName="riskprox" color="#ed1c24" page="/wallet/leveraged" colorAmount={""} />
+                <Row lg={12}>
+                    <Col lg={4} className="mrc-3">
+                        <WalletBalance/>
+                    </Col>
+                    <Col lg={18} style={{ width: '100%' }}>
+                        <TokenSummaryCard tokenName="stable" color="#00a651" page="/wallet/stable" colorAmount={""} />
+                        <TokenSummaryCard tokenName="riskpro" color="#ef8a13" page="/wallet/pro" colorAmount={""} />
+                        <TokenSummaryCard tokenName="riskprox" color="#ed1c24" page="/wallet/leveraged" colorAmount={""} />
+                    </Col>
+                </Row>
             </div>
         </Fragment>
     )
