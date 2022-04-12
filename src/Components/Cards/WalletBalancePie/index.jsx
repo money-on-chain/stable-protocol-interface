ef57071ea10ca0c8a85bd97223ca8d5c9dafbce4
javier <inf.javier.gonzales@gmail.com>
2022-04-12 12:26:12 -0400

    añadir tooltips al piechart como en el sitio anterior

--- src/Components/Cards/WalletBalancePie/index.jsx
+++ src/Components/Cards/WalletBalancePie/index.jsx
@@ -2,14 +2,42 @@ import React, { PureComponent } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip} from 'recharts';
 
 const data = [
-    { name: 'Group A', value: 200 },
-    { name: 'Group B', value: 800 },
-    { name: 'Group C', value: 300 }
+    { name: 'Group A', value: 200, set1: '27366633 RBTC', set2: '27366633 DOC', class: 'STABLE' },
+    { name: 'Group B', value: 800, set1: '2736 RBTC', set2: '887127 BPRO', class: 'RISKPRO' },
+    { name: 'Group C', value: 300, set1: '363443 RBTC', set2: '41233 RBTC', class: 'RISKPROX' }
 ];
 const COLORS = ['#00a651', '#00C49F', '#ef8a13'];
 
+const getIntroOfPage = (label) => {
+    if (label === 'Group A') {
+        return "Page A is about men's clothing";
+    }
+    if (label === 'Group B') {
+        return "Page B is about women's dress";
+    }
+    if (label === 'Group  C') {
+        return "Page C is about women's bag";
+    }
+    return '';
+};
+
+const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+        return (
+            <div className="custom-tooltip pieChartTooltip">
+                {/*<p className="label">{`${label} : ${payload[0].value}`}</p>*/}
+                {/*<p className="intro">{getIntroOfPage(label)}</p>*/}
+                <p className="value-1">{`${payload[0].payload.set1}`}</p>
+                <p className={`${payload[0].payload.class}`}>{`${payload[0].payload.set2}`}</p>
+            </div>
+        );
+    }
+
+    return null;
+};
+
 export default class WalletBalancePie extends PureComponent {
-    static demoUrl = 'https://codesandbox.io/s/pie-chart-with-padding-angle-7ux0o';
+    // static demoUrl = 'https://codesandbox.io/s/pie-chart-with-padding-angle-7ux0o';
 
     render() {
         return (
@@ -28,6 +56,7 @@ export default class WalletBalancePie extends PureComponent {
                         <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                     ))}
                 </Pie>
+                <Tooltip content={<CustomTooltip />} />
             </PieChart>
                 </ResponsiveContainer>
                 <span className={'money-RBTC'}>786362778 RBTC</span>
