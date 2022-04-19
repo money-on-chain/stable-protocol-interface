1002303671d397df7c44cc25edc4b0dd964197ac
Micaela Vandoni <mtvandoni@gmail.com>
2022-04-19 15:32:07 -0300

    add cards next settlement and leverage

--- src/Pages/Mint/MintLeveraged/index.jsx
+++ src/Pages/Mint/MintLeveraged/index.jsx
@@ -1,7 +1,8 @@
 import MintCard from '../../../Components/Cards/MintCard';
 import AmountCard from '../../../Components/Cards/AmountCard';
 import YourAddressCard from '../../../Components/Cards/YourAddressCard';
-import { Row, Col, Switch } from 'antd';
+import { Row, Col, Tooltip } from 'antd';
+import { InfoCircleOutlined } from '@ant-design/icons';
 import { Fragment } from 'react';
 import TokenSummaryCard from '../../../Components/Cards/TokenSummaryCard';
 
@@ -10,18 +11,53 @@ export default function Mint(props) {
         <Fragment>
             <h1 className="PageTitle">BTCx</h1>
             <h3 className="PageSubTitle">Manage your BTCx Position</h3>
-            <div className="WalletCards">
-                <AmountCard tokenName="riskprox" titleName="BTCx"/>
-                <YourAddressCard/>
-                <MintCard
-                    token={'RISKPROX'}
-                    AccountData={props.Auth.accountData}
-                    UserBalanceData={props.Auth.userBalanceData}
-                    StatusData={props.Auth.contractStatusData}
-                    currencyOptions={['RESERVE', 'RISKPROX']}
-                    color="#ed1c24"
-                />
-            </div>
+            <Row gutter={15}>
+                <Col span={5}>
+                    <AmountCard tokenName="riskprox" titleName="BTCx"/>
+                </Col>
+                <Col span={5}>
+                    <Row>
+                        <Col span={24}>
+                            <div className="Card MintCard CardSettlement">
+                                <h3 className="CardTitle">Next settlement</h3>
+                                <Row>
+                                    <h2>In 1 days 9 hpurs</h2>
+                                    <div className="CaptionDateSettlement">April 20th 2022, 11:39:22 pm</div>
+                                    <div>
+                                        <span className="SettlementTitle">Remaining blocks: </span>
+                                        4004
+                                        <Tooltip placement="top" title="200849">
+                                            <InfoCircleOutlined className="Icon"/>
+                                        </Tooltip>
+                                    </div>
+                                </Row>
+                            </div>
+                        </Col>
+                        <Col span={24} style={{ marginTop: '1em' }}>
+                            <div className="Card MintCard Bprox2Metrix">
+                                <h3 className="CardTitle">BTCx Leverage</h3>
+                                <div>
+                                    <span>2.0000</span>
+                                </div>
+                                <h3 className="CardTitle">Current Interest Rate</h3>
+                                <div>
+                                    <span>0.027379</span>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col span={14}>
+                    <MintCard
+                        token={'RISKPROX'}
+                        AccountData={props.Auth.accountData}
+                        UserBalanceData={props.Auth.userBalanceData}
+                        StatusData={props.Auth.contractStatusData}
+                        currencyOptions={['RESERVE', 'RISKPROX']}
+                        color="#ed1c24"
+                    />
+                </Col>
+            </Row>
         </Fragment>
     );
 }
