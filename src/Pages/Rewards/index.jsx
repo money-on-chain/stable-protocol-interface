d6f7fc0c50ebd42b7c8e2f2deab1d9a2e54f531a
mvandoni <micaela.vandoni@mercadolibre.com>
2022-04-23 19:33:20 -0300

    add rewardsBalanceCard and youradress with responsive

--- src/Pages/Rewards/index.jsx
+++ src/Pages/Rewards/index.jsx
@@ -1,10 +1,21 @@
 import { Fragment } from "react";
+import { Row, Col } from 'antd';
+import RewardsBalanceCard from "../../Components/Cards/RewardsBalanceCard";
+import YourAddressCard from '../../Components/Cards/YourAddressCard';
 
 export default function Rewards(props) {
     return (
         <Fragment>
             <h1 className="PageTitle">MoC</h1>
             <h3 className="PageSubTitle">Manage your BPros</h3>
+            <Row gutter={15}>
+                <Col xs={24} md={12} xl={5}>
+                    <RewardsBalanceCard />
+                </Col>
+                <Col xs={24} md={12} xl={4}>
+                    <YourAddressCard/>
+                </Col>
+            </Row>
         </Fragment>
     )
 }
