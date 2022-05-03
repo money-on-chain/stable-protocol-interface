17b8b516421a88cd76c5b687e73d31880e19aa70
Jose Luis <sisjosex@gmail.com>
2022-05-02 20:33:09 -0400

    pie-chart-metrics

--- src/Components/Cards/Metrics/RBTC/index.jsx
+++ src/Components/Cards/Metrics/RBTC/index.jsx
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
 import { Col, Row } from 'antd';
-import { Cell, Pie } from 'recharts';
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts';
 import { AuthenticateContext } from '../../../../Context/Auth';
 
-const COLORS = ['#00a651', '#ef8a13'];
+const COLORS = ['#ef8a13', '#00a651'];
 
 function RBTC() {
     const auth = useContext(AuthenticateContext);
@@ -33,29 +33,34 @@ function RBTC() {
 
             <div className="CardMetricContent">
                 <div>
-                    <h3 style={{textAlign: 'center'}}>Total rBTC in the system</h3>
+                    <h3 style={{ textAlign: 'center' }}>Total rBTC in the system</h3>
+                    <div style={{ height: 180, width: 180, margin: '0 auto' }}>
+                        <ResponsiveContainer style={{marginLeft: '0 !important'}}>
+                            <PieChart>
+                                <Pie
+                                    data={getPie()}
+                                    innerRadius={40}
+                                    outerRadius={90}
+                                    fill="#8884d8"
+                                    paddingAngle={1}
+                                    dataKey="value"
+                                >
+                                    {getPie() !== undefined &&
 
-                    <Pie
-                        data={getPie()}
-                        innerRadius={115}
-                        outerRadius={120}
-                        fill="#8884d8"
-                        paddingAngle={1}
-                        dataKey="value"
-                    >
-                        {getPie() !== undefined &&
+                                    getPie().map((entry, index) => (
+                                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
+                                    ))
 
-                        getPie().map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
-                        ))
-
-                        }
-                    </Pie>
-                    <div style={{textAlign: 'center'}}>5.774093 RBTC</div>
-                    <h3 style={{textAlign: 'center'}}>222,309.98 USD</h3>
+                                    }
+                                </Pie>
+                            </PieChart>
+                        </ResponsiveContainer>
+                    </div>
+                    <div style={{ textAlign: 'center' }}>5.774093 RBTC</div>
+                    <h3 style={{ textAlign: 'center' }}>222,309.98 USD</h3>
                 </div>
-                <div className="separator" style={{height: 220}} />
-                <div style={{marginLeft: 30}}>
+                <div className="separator" style={{ height: 220 }}/>
+                <div style={{ marginLeft: 30 }}>
                     <h3>rBTC USD</h3>
                     38,621.60 USD
                     <h3>rBTC in interest bag</h3>
