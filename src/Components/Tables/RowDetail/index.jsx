15e8b82290a5afe921eef9dbf3c099090444487f
andrea flores jaldin <floresjaldinandrea@gmail.com>
2022-05-26 18:34:33 -0400

    Translations into ListOperations

--- src/Components/Tables/RowDetail/index.jsx
+++ src/Components/Tables/RowDetail/index.jsx
@@ -2,6 +2,7 @@
 import React, { Fragment, useState } from 'react';
 import { useContext } from 'react'
 import { AuthenticateContext } from "../../../Context/Auth";
+import { useTranslation } from "react-i18next";
 import Web3 from 'web3'
 const BigNumber = require('bignumber.js');
 
@@ -10,17 +11,18 @@ const BigNumber = require('bignumber.js');
 function RowDetail(props) {
 
     const auth = useContext(AuthenticateContext);
+    const [t, i18n] = useTranslation(["global", 'moc']);
 
     return (
 
         <table>
             <tbody>
                 <tr className="ant-descriptions-row">
-                    <th className="ant-descriptions-item-label-th ant-descriptions-border-bottom" colSpan="1">Event</th>
+                    <th className="ant-descriptions-item-label-th ant-descriptions-border-bottom" colSpan="1">{t('MoC.operations.columns_detailed.event', { ns: 'moc' })}</th>
                     <td className="ant-descriptions-item-content ant-descriptions-border-bottom" colSpan="1">{props.detail.event}     </td>
-                    <th className="ant-descriptions-item-label-th ant-descriptions-border-bottom" colSpan="1">Created</th>
-                    <td className="ant-descriptions-item-content ant-descriptions-border-bottom" colSpan="1">{props.detail.created} </td>
-                    <th className="ant-descriptions-item-label-th ant-descriptions-border-bottom" colSpan="1">Detail</th>
+                    <th className="ant-descriptions-item-label-th ant-descriptions-border-bottom" colSpan="1">{t('MoC.operations.columns_detailed.created', { ns: 'moc' })}</th>
+                    <td className="ant-descriptions-item-content ant-descriptions-border-bottom" colSpan="1">{props.detail.created}</td>
+                    <th className="ant-descriptions-item-label-th ant-descriptions-border-bottom" colSpan="1">{t('MoC.operations.columns_detailed.detail', { ns: 'moc' })}</th>
                     <td className="ant-descriptions-item-content ant-descriptions-border-bottom" colSpan="1">
                         <div>{props.detail.details} </div>
                     </td>
