21c6d3ccf802339c203b5e945a79055285a5e19a
Wilder_Canaviri_Astete <wildersistemas20@gmail.com>
2022-05-31 18:42:21 -0400

    sorting and filter in data_json, fix pagination

--- src/Helpers/helper.jsx
+++ src/Helpers/helper.jsx
@@ -161,3 +161,8 @@ export const toNumberFormat = (value, decimals = 0) => {
         minimumFractionDigits: decimals,
     });
 }
+
+export const myParseDate = date_string => {
+    let [y,M,d,h,m,s] = date_string.split(/[- :T]/);
+    return new Date(y,parseInt(M)-1,d,h,parseInt(m),s.replace('Z',''));
+}
