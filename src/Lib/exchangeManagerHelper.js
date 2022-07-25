ca5303af5addab04ba1ba81de84494ae46fc826c
Micaela Vandoni <mtvandoni@gmail.com>
2022-07-25 17:39:31 -0300

    15. vendor from config

--- src/Lib/exchangeManagerHelper.js
+++ src/Lib/exchangeManagerHelper.js
@@ -6,6 +6,7 @@ import {
 } from './Formats';
 import { toBigNumber, minimum } from './numberHelper';
 import { getTransactionType } from './exchangeHelper';
+import { config } from '../Config/config';
 
 const BigNumber = require('bignumber.js');
 
@@ -143,7 +144,8 @@ const getCommissionRateAndCurrency = ({currencyYouExchange, currencyYouReceive, 
   } = mocState || {};
   if(!convertToken) return {};
   
-  const vendor = { address: "0xf69287F5Ca3cC3C6d3981f2412109110cB8af076", markup: "500000000000000" };
+  // const vendor = { address: "0xf69287F5Ca3cC3C6d3981f2412109110cB8af076", markup: "500000000000000" };
+  const vendor = config.vendor;
 
   const valueYouExchangeInRESERVE = convertToken(currencyYouExchange, "RESERVE", valueYouExchange);
   const valueYouExchangeInMOC = convertToken("RESERVE", "MOC", valueYouExchangeInRESERVE);
