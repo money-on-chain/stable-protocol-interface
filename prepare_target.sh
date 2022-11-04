6dcc6bb267ea46090b05b2cd4f7d19c235313920
mulonemartin <mulone.martin@gmail.com>
2022-11-04 14:01:10 -0300

    Prepare target copying files to dist folders

---
+++ prepare_target.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Preparing to run project: $1"
+
+# public
+echo "Copying public files ..."
+cp public/projects/$1/*.* public/
+cp public/projects/$1/icons/ public/ -R
+cp public/projects/$1/img/ public/ -R
+echo "Copying public files done!"
+
+# Assets
+echo "Copying assets files ..."
+cp src/assets/projects/$1/css/ src/assets/ -R
+cp src/assets/projects/$1/fonts/ src/assets/ -R
+cp src/assets/projects/$1/icons/ src/assets/ -R
+echo "Copying assets files done!"
+
+# Contracts
+echo "Copying contract files ..."
+cp src/Contracts/projects/$1/*.* src/Contracts/
+cp src/Contracts/projects/$1/omoc/ src/Contracts/ -R
+echo "Copying assets files done!"
+
+echo "Done preparing project! "
