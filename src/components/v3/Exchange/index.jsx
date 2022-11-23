62c15a139e8064e01d626ee8c576cb034418aa71
mulonemartin <mulone.martin@gmail.com>
2022-11-23 16:16:30 -0300

    Flipago UI - Exchange

---
+++ src/components/v3/Exchange/index.jsx
@@ -0,0 +1,111 @@
+
+import { Switch, Button } from 'antd';
+import React, { useContext, useState, useEffect } from 'react';
+
+import {useProjectTranslation} from "../../../helpers/translations";
+import {config} from "../../../projects/config";
+import SelectCurrency from "../../v3/SelectCurrency";
+
+
+export default function Exchange() {
+    const [t, i18n, ns] = useProjectTranslation();
+    const AppProject = config.environment.AppProject;
+
+    return (
+    <div className="exchange-content">
+        <div className="exchange-content-fields">
+            <div className="exchange-swap-from">
+
+                <SelectCurrency
+                    className="exchange-select-token"
+                    disabled={false}
+                    inputValueInWei={0.00}
+                    value={'RESERVE'}
+                    currencySelected={'RESERVE'}
+                    currencyOptions={['RESERVE', 'TC', 'TP']}
+                />
+
+                <input className="exchange-input-value" type="text" id="select-token-from" name="select-token-from" placeholder="123.4" />
+
+                <div className="exchange-token-balance">
+                            <span className="exchange-token-balance-value">
+                                Balance: 1234.12
+                            </span>
+                    <a href="#" className="exchange-token-balance-add-total">Add total available</a>
+
+                </div>
+
+
+            </div>
+            <div className="exchange-swap-arrow">
+                <i className="icon-swap-arrow"></i>
+            </div>
+
+            <div className="exchange-swap-to">
+
+                <SelectCurrency
+                    className="exchange-select-token"
+                    disabled={false}
+                    inputValueInWei={0.00}
+                    value={'TP'}
+                    currencySelected={'TP'}
+                    currencyOptions={['RESERVE', 'TC', 'TP']}
+                />
+
+                <input className="exchange-input-value" type="text" id="select-token-from" name="select-token-from" placeholder="123.4" />
+
+                <div className="exchange-token-balance">
+                            <span className="exchange-token-balance-value">
+                                Balance: 1234.12
+                            </span>
+
+                    <a href="#" className="exchange-token-balance-add-total">Add total available</a>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+        <div className="exchange-content-info">
+        <div className="exchange-content-info-prices">
+            <div className="exchange-content-info-conversion_0">1 FlipMega ≈ 1.0323 Dollar On Chain</div>
+            <div className="exchange-content-info-conversion_1">1 Dollar On CHain ≈ 0.9323 FlipMega</div>
+        </div>
+
+        <div className="exchange-content-info-fees">
+            <div className="exchange-content-info-fees-frame">
+                <div className="exchange-content-info-fees-frame-t">
+                    Fee (0.15%) ≈ 0.0000342 rBTC
+                </div>
+
+                <div className="exchange-content-info-fees-frame-switch">
+                    <Switch
+                        disabled={false}
+                        checked={false}
+                    />
+
+                </div>
+            </div>
+            <div className="exchange-content-info-fees-balance">
+                This fee will be deducted from the transaction value transfered. Amounts my be different at transaction confirmation.
+            </div>
+
+        </div>
+
+        <div className="exchange-content-info-cta">
+                        <span className="exchange-content-info-cta-exchanging">
+                            Exchanging ≈ 132.15 USD
+                        </span>
+
+            <Button
+                type="primary"
+                className="exchange-content-info-cta-button"
+            > Exchange </Button>
+
+        </div>
+    </div>
+    </div>
+    )
+}
--- src/components/v3/Modals/Exchange/index.jsx
+++ src/components/v3/Modals/Exchange/index.jsx
@@ -1,122 +1,43 @@
-
-import { Switch, Button } from 'antd';
 import React, { useContext, useState, useEffect } from 'react';
+import Modal from 'antd/lib/modal/Modal';
 
 import {useProjectTranslation} from "../../../../helpers/translations";
 import {config} from "../../../../projects/config";
-
+import Exchange from "../../../v3/Exchange";
+import {Button} from "antd";
 
 
 export default function ModalExchange() {
     const [t, i18n, ns] = useProjectTranslation();
     const AppProject = config.environment.AppProject;
+    const [visible, setVisible] = useState(false);
+
+    const showModal = () => {
+        setVisible(true);
+    };
+
+    const hideModal = () => {
+        setVisible(false);
+    };
 
     return (
-        <div className="modal-exchange">
-            <div className="modal-exchange-header">
-                <h1>Exchange</h1>
-                <div className="modal-close">
-                    <i className="icon-modal-close"></i>
-                </div>
-            </div>
+        <div>
+            <Button
+                type="primary"
+                onClick={showModal}
+            >{t(`${AppProject}.wallet.send`, { ns: ns })}
+            </Button>
+            <Modal
+                title="Exchange"
+                width={650}
+                visible={visible}
+                onCancel={hideModal}
+                footer={null}
+                className="ModalExchange"
+            >
+                <Exchange/>
 
-            <div className="modal-exchange-content">
-                <div className="exchange-content-fields">
-                    <div className="exchange-swap-from">
-
-                        <select className="exchange-select-token">
-                            <option key="FLIPM" value="FlipMega">
-                                <div className="exchange-currency-option">
-                                    <i className="icon-token-tc"></i>
-                                    <span className="exchange-currency-caption">FlipMega</span>
-                                </div>
-                            </option>
-                        </select>
-
-                        <input className="exchange-input-value" type="text" id="select-token-from" name="select-token-from" placeholder="123.4" />
-
-                        <div className="exchange-token-balance">
-                            <span className="exchange-token-balance-value">
-                                Balance: 1234.12
-                            </span>
-                            <a href="#" className="exchange-token-balance-add-total">Add total available</a>
-
-
-                        </div>
-
-
-                    </div>
-                    <div className="exchange-swap-arrow">
-                        <i className="icon-swap-arrow"></i>
-                    </div>
-
-                    <div className="exchange-swap-to">
-
-                        <select className="exchange-select-token">
-                            <option key="DOC" value="Dollar on Chain">
-                                <div className="exchange-currency-option">
-                                    <i className="icon-token-tp_0"></i>
-                                    <span className="exchange-currency-caption">Dollar on chain</span>
-                                </div>
-                            </option>
-                        </select>
-
-                        <input className="exchange-input-value" type="text" id="select-token-from" name="select-token-from" placeholder="123.4" />
-
-                        <div className="exchange-token-balance">
-                            <span className="exchange-token-balance-value">
-                                Balance: 1234.12
-                            </span>
-
-                            <a href="#" className="exchange-token-balance-add-total">Add total available</a>
-
-                        </div>
-
-
-                    </div>
-
-                </div>
-
-                <div className="exchange-content-info">
-                    <div className="exchange-content-info-prices">
-                        <div className="exchange-content-info-conversion_0">1 FlipMega ≈ 1.0323 Dollar On Chain</div>
-                        <div className="exchange-content-info-conversion_1">1 Dollar On CHain ≈ 0.9323 FlipMega</div>
-                    </div>
-
-                    <div className="exchange-content-info-fees">
-                        <div className="exchange-content-info-fees-frame">
-                            <div className="exchange-content-info-fees-frame-t">
-                                Fee (0.15%) ≈ 0.0000342 rBTC
-                            </div>
-
-                            <div className="exchange-content-info-fees-frame-switch">
-                                <Switch
-                                    disabled={false}
-                                    checked={false}
-                                />
-
-                            </div>
-                        </div>
-                        <div className="exchange-content-info-fees-balance">
-                            This fee will be deducted from the transaction value transfered. Amounts my be different at transaction confirmation.
-                        </div>
-
-                    </div>
-
-                    <div className="exchange-content-info-cta">
-                        <span className="exchange-content-info-cta-exchanging">
-                            Exchanging ≈ 132.15 USD
-                        </span>
-
-                        <Button
-                            type="primary"
-                            className="exchange-content-info-cta-button"
-                        > Exchange </Button>
-
-                    </div>
-                </div>
-
-            </div>
+            </Modal>
         </div>
     )
 }
