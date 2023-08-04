1f379c088cc81e48cfac71ef29d418879dc2729f
mulonemartin <mulone.martin@gmail.com>
2023-08-04 09:10:06 -0300

    fix: list operation new tg transfer

--- src/components/Tables/ListOperations/index.jsx
+++ src/components/Tables/ListOperations/index.jsx
@@ -432,6 +432,24 @@ export default function ListOperations(props) {
                         { ns: ns }
                     );
                     break;
+                case 'TG':
+                    asset.push({
+                        image: (
+                            <LogoIconTG
+                                className="uk-preserve-width uk-border-circle"
+                                alt="avatar"
+                                width="32"
+                                height="32"
+                            />
+                        ),
+                        color: 'color-token-tg',
+                        txt: 'MOC'
+                    });
+                    data_row_coins2[index].detail.asset = t(
+                        `${AppProject}.Tokens_TG_code`,
+                        { ns: ns }
+                    );
+                    break;
                 default:
                     asset.push({
                         image: (
