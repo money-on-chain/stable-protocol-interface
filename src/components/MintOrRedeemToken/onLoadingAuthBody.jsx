0f82b89e9050e0b6f5f4c14fda7be72b6f85bc5f
Sebas <sheva3232@gmail.com>
2024-02-08 12:13:39 -0500

    fix(white screen): adding loading helper Task ROC-743

---
+++ src/components/MintOrRedeemToken/onLoadingAuthBody.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Card,
+  Skeleton
+} from 'antd';
+const OnLoadingAuthBody = (props) => {
+  const { title } = props;
+    return (
+        <Card
+            title={title}
+            className="Card MintOrRedeemToken"
+            style={{ height: '100%' }}
+        >
+            <Skeleton active={true} paragraph={{ rows: 6 }}></Skeleton>
+        </Card>
+    );
+};
+
+export default OnLoadingAuthBody;
